9ac86b460d16abdc12b1c37ef8766aea7fc45256
Tejas <tejaspithva03@gmail.com>
2025-11-01 18:18:32 +0530

    started of nodeJs

---
+++ NodeJs/another.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sum = (n1,n2) => n1 + n2;
+// const PI = 3.14
+// class SumMath{
+//     constructor(){
+//         console.log('obj created')
+//     }
+// }
+// module.exports.sum = sum
+// module.exports.PI = PI
+// module.exports.SumMath = SumMath
+
+// module.exports = {sum : sum, PI : PI, SumMath : SumMath}
