da3d8d4c5b2d764272f371040db385fd47556a6a
Tejas <tejaspithva03@gmail.com>
2025-11-08 14:56:45 +0530

    done

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,1 +1,56 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+app.get('/',function(req,res){
+    res.send("Welcome to my hotel")
+})
+
+app.get('/panjabi',(req,res)=>{
+    res.send('sure sir, i would love to serve panjabi')
+})
+
+app.get('/dhosa',(req,res)=>{
+    var coustomise_dhosa = {
+        name: "Maisur dhosa",
+        size: "55cm diamitter",
+        sambhar: true,
+        chatni: true
+    }
+    res.send(coustomise_dhosa)
+    res.send('sure sir,welcome to south india, i would love to serve dhosa')
+})
+
+app.post('/items',(req,res)=>{
+    console.log("data is seved")
+    res.send("data is seved")
+})
+
+app.listen(3000,()=>{
+    console.log(`server is running in port: 3000`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("hello worldddd")
+// var fs  = require("fs")
+// var os  = require("os")
+
+// var user = os.userInfo()
+// console.log(user)
+// console.log(user.username)
+// console.log("hi")
+
